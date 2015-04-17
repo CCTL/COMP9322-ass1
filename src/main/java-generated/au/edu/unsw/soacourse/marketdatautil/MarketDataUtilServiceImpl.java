68c32677b4c4fe18940c9c2de97122bd9e7c239e
Albert Wang <albertwang2002@hotmail.com>
2015-04-17 12:40:24 +1000

    Tested and fixed summarise

--- src/main/java-generated/au/edu/unsw/soacourse/marketdatautil/MarketDataUtilServiceImpl.java
+++ src/main/java-generated/au/edu/unsw/soacourse/marketdatautil/MarketDataUtilServiceImpl.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class MarketDataUtilServiceImpl implements MarketDataUtilService {
 
@@ -45,7 +47,7 @@ public class MarketDataUtilServiceImpl implements MarketDataUtilService {
 			String[] secInfo = null;
 			while ((line = br.readLine()) != null) {
 				secInfo = line.split(",");
-				if (currentLine == 0) {
+				if (currentLine == 1) {
 					response.setEndDate(secInfo[1]);
 				}
 				currentLine++;
@@ -53,9 +55,27 @@ public class MarketDataUtilServiceImpl implements MarketDataUtilService {
 			if (secInfo != null) {
 				response.setStartDate(secInfo[1]);
 				response.setSec(secInfo[0]);
-				response.setCurrencyCode(secInfo[2].substring(0, 2));
+				response.setCurrencyCode(secInfo[2].substring(0, 3));
 				response.setEventSetId(parameters.getEventSetId());
 				response.setFileSize((file.length() / 1024) + "K");
+				
+				// Flips the date around
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+				SimpleDateFormat sdfResult = new SimpleDateFormat("dd-MM-yyyy");
+				try {
+					response.setStartDate(sdfResult.format(sdf.parse(response.getStartDate())));
+					response.setEndDate(sdfResult.format(sdf.parse(response.getEndDate())));
+				} catch (ParseException e) {
+					String msg = "Invalid date format";
+					String code= "DATE_ERR";
+					  
+					ServiceFaultType fault = factory.createServiceFaultType();
+					fault.setErrcode(code);
+					fault.setErrtext(msg);
+					  
+					e.printStackTrace();
+					throw new SummariseMarketDataFaultMsg(msg,fault);
+				}
 			}
 		} catch (FileNotFoundException e) {
 			throw new SummariseMarketDataFaultMsg("EventSetID is invalid");
